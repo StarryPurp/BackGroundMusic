f37d8028d1ac1bfa87805e8426e7dbd3922a048d
StarryPurp <starbloom128@gmail.com>
2017-06-19 19:17:01 +0900

    #3 Music Event

--- app/src/main/java/bstar128/example/hansung/backgroundmusic/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/backgroundmusic/MainActivity.java
@@ -1,13 +1,37 @@
 package bstar128.example.hansung.backgroundmusic;
 
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switchmusic;
+    MediaPlayer musicp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchmusic=(Switch) findViewById(R.id.switch_use);
+        musicp=MediaPlayer.create(this,R.raw.reverse);
+        switchmusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchmusic.isChecked()){
+                    musicp.start();
+                }
+                else{
+                    musicp.pause();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        musicp.stop();
     }
 }
